f6345ad720a638d3a8a7ead2e3a4a6945e9d80ce
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2021-09-28 13:07:12 +1300

    Switching from Doom emacs to GNU emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,307 @@
+;;; package --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
+;; No flashing or alerting
+(setq inhibit-startup-message t)
+(setq ring-bell-function 'ignore)
+
+(scroll-bar-mode -1)    ; Remove bar
+(tool-bar-mode -1)      ; Disable the tool bar
+(tooltip-mode -1)       ; Disable tooltips
+(set-fringe-mode 10)    ; Provide breathing room
+
+(set-face-attribute 'default nil :font "Fira Code" :height 125)
+
+;;; Line numbers
+(global-display-line-numbers-mode)
+(setq display-line-numbers-type 'visual)
+(dolist (mode '(org-mode-hook
+		dired-mode-hook
+		image-mode-hook
+                term-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Initialize package sources
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+;; Write backups to ~/.emacs.d/backup/
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+      backup-by-copying      t  ; Don't de-link hard links
+      version-control        t  ; Use version numbers on backups
+      delete-old-versions    t  ; Automatically delete excess backups:
+      kept-new-versions      20 ; how many of the newest versions to keep
+      kept-old-versions      5) ; and how many of the old
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;;; Dashboard
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  (dashboard-refresh-buffer))
+
+(setq dashboard-center-content t)
+(setq dashboard-startup-banner "./Dragonfly.png")
+
+;; Get counsel
+(use-package counsel
+  :ensure t
+  :config (counsel-mode))
+
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-k") 'kill-this-buffer)
+
+;; Setup Ivy
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+;; Get theme
+(use-package kaolin-themes
+  :config
+  (load-theme 'kaolin-bubblegum t))
+
+(setq kaolin-themes-bold t
+      kaolin-themes-italic t
+      kaolin-themes-underline t)
+
+;; Modeline
+(use-package all-the-icons)
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 5)))
+
+;;; Command log mode
+(use-package command-log-mode)
+
+;;; Which key
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle 0.3))
+
+;;; Evil
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+
+;;; Org configuration
+(setq org-directory "~/org/")
+(setq org-agenda-files (directory-files-recursively "~/org/" "\\.org$"))
+
+(defun dw/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (auto-fill-mode 0)
+  (visual-line-mode 1)
+  (setq evil-auto-indent nil))
+
+(use-package org
+  :hook (org-mode . dw/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾"
+        org-hide-emphasis-markers t))
+
+(setq org-hide-emphasis-markers t)
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+;; Replace list hyphen with dot
+(font-lock-add-keywords 'org-mode
+                         '(("^ *\\([-]\\) "
+                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+;; Make sure org-indent face is available
+(require 'org-indent)
+
+;; Org headings
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.4))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.3))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.2))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.1)))))
+
+;;; Keybindings
+(use-package general
+  :ensure t
+  :config
+  (general-create-definer pt/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (pt/leader-keys
+   "t"  '(:ignore t :which-key "toggles")
+   "tt" '(counsel-load-theme :which-key "choose theme")))
+
+;;; Hydra
+(use-package hydra)
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(pt/leader-keys
+  "ts" '(hydra-text-scale/body :which-key "scale text"))
+
+;;; Projectile
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "~/Developer/Projects")
+    (setq projectile-project-search-path '("~/Developer/Projects")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+;;; Skeletor
+(use-package skeletor
+  :config
+  (setq skeletor-completing-read-function 'ivy-completing-read
+	skeletor-project-directory "~/Developer/Projects"
+	skeletor--project-types nil))
+
+(skeletor-define-template "python-project"
+  :title "Python Project"
+  :default-license (rx bol "gpl"))
+
+;;; Magit
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+;;;; Programming modes
+
+;;; Python Setup
+(use-package elpy
+	 :ensure t
+	 :init
+	 (elpy-enable))
+
+(use-package auto-virtualenv
+ :ensure t)
+
+;;;; Web Setup
+(use-package web-mode
+  :ensure t)
+
+;; Javascript
+(use-package js2-mode
+  :ensure t)
+
+;; Scss support
+(use-package scss-mode
+  :ensure t)
+
+;;; Rust Setup
+(use-package rust-mode
+  :ensure t)
+
+;;; Snippets
+(use-package yasnippet-snippets
+  :ensure t)
+
+;;; Autocomplete
+(use-package company
+  :ensure t
+  :init (company-mode))
+
+;;; Spell Check
+(use-package flycheck
+  :ensure t
+  :init
+  (global-flycheck-mode))
+
+;;; Workspaces
+(use-package persp-mode
+  :ensure t)
+
+;;; Pdf tools
+(use-package pdf-tools
+  :ensure t
+  :init (pdf-tools-install))
+
+;;; Rainbow deliminators
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(rainbow-delimiters pdf-tools persp-mode flycheck yasnippet-snippets rust-mode scss-mode js2-mode web-mode auto-virtualenv elpy magit skeletor counsel-projectile projectile hydra general org-bullets evil-collection evil which-key command-log-mode doom-modeline all-the-icons kaolin-themes counsel dashboard use-package)))
