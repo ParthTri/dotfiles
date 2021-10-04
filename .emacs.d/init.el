f9f585185f6c07f8284052646978dab27d77d187
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2021-10-04 23:04:44 +1300

    Org roam setup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -242,6 +242,21 @@
   :ensure t
   :config (add-hook 'org-mode-hook 'toc-org-mode))
 
+;;; Org roam
+(use-package org-roam
+  :ensure t
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory "~/RoamNotes")
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert)
+         :map org-mode-map
+         ("C-M-i"    . completion-at-point))
+  :config
+  (org-roam-setup))
 
 ;;; Hydra
 (use-package hydra)
