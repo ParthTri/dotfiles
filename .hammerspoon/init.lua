a12754f95cd31bac35ca070336bf098df73c08ba
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2021-12-13 11:16:14 +1300

    Setting up basic window management with hammerspoon

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,72 @@
+hs.loadSpoon("SpoonInstall")
+
+hyper = {"ctrl", "shift", "cmd", "alt"}
+
+hs.hotkey.bindSpec( {hyper, "y"}, hs.toggleConsole)
+hs.hotkey.bindSpec( {hyper, "r"}, hs.reload)
+
+hs.hotkey.bind(hyper, "h", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind(hyper, "j", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y + (max.h / 2)
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind(hyper, "k", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind(hyper, "l", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Set app to full screen with padding
+hs.hotkey.bind(hyper, "Space", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + 10 
+  f.y = max.y + 10
+  f.w = max.w - 20
+  f.h = max.h - 20
+  win:setFrame(f)
+end)
