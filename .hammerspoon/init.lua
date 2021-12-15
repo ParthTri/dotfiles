8a09745d179a23a16c5f79c3794e1a2981aaf743
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2021-12-15 13:44:27 +1300

    Created keybindings to switch focus of apps

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -19,3 +19,33 @@ spoon.WindowScreenLeftAndRight:bindHotkeys({
       screen_left = {hyper, "h"},
       screen_right = {hyper, "l"},
 })
+
+hs.hotkey.bind("ctrl", "k", function ()
+		   local win = hs.window.focusedWindow()
+		   local apps = hs.window.allWindows()
+		   local curr = 1
+
+		   for i = 1, #apps do
+		      if win == apps[i] then curr = i end
+		   end
+
+		   if curr == #apps then curr = 0 end
+		   print (apps[curr + 1])
+		   apps[curr + 1]:focus()
+		   print (hs.window.focusedWindow())
+end )
+
+hs.hotkey.bind("ctrl", "j", function ()
+		   local win = hs.window.focusedWindow()
+		   local apps = hs.window.allWindows()
+		   local curr = 1
+
+		   for i = 1, #apps do
+		      if win == apps[i] then curr = i end
+		   end
+		   if curr == 1 then curr = #apps end
+
+		   apps[curr - 1]:focus() 
+		   print (hs.window.focusedWindow())
+end )
+
