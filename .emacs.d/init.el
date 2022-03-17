f9e5eb6f75c812fc539ff626e6d3342d8fc08fed
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2022-03-17 20:52:46 +1300

    Adding lsp jedi server access

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -463,6 +463,13 @@
   :config
   (pyvenv-mode 1))
 
+(use-package lsp-jedi
+  :ensure t
+  :config
+  (with-eval-after-load "lsp-mode"
+    (add-to-list 'lsp-disabled-clients 'pyls)
+    (add-to-list 'lsp-enabled-clients 'jedi)))
+
 ;;; Lua Setup
 (use-package lua-mode
   :ensure t)
