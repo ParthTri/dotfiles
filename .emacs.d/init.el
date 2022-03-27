9e28583ca77963be2ff3a1df43d371829dea687c
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2022-03-27 17:14:31 +1300

    Fixing use-package tangle to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -30,6 +30,23 @@
                 vterm-mode-hook))
                 (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
+;; Use package
+
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+    (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+    (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
 ;; Backups
 ;; Write backups to ~/.emacs.d/backup/
 
