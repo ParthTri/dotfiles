3667811bed9621c11014f20d2e3600027d2f6504
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2022-04-02 20:47:41 +1300

    Making transparency more readable

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -233,8 +233,8 @@
 
 ;; Transparency
 
-(set-frame-parameter (selected-frame) 'alpha '(65 50))
-(add-to-list 'default-frame-alist '(alpha 65 50))
+(set-frame-parameter (selected-frame) 'alpha '(85 50))
+(add-to-list 'default-frame-alist '(alpha 85 50))
 
 ;; Org Configuration
 
