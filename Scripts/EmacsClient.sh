94a6bb1cc2efddcca42e7e44b433a7c82af91264
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2022-04-23 16:01:41 +1200

    Adding raycast scripts

---
+++ Scripts/EmacsClient.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Emacs Client
+# @raycast.mode inline
+# @raycast.packageName Raycast Scripts
+#
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.currentDirectoryPath ~
+# @raycast.needsConfirmation false
+#
+# Documentation:
+# @raycast.description Script command to open a new emacs client frame
+# @raycast.author Parth Trivedi
+# @raycast.authorURL An URL for one of your social medias#!/bin/bash
+
+emacsclient -c
