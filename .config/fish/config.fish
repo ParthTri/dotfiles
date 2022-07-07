6055de7c4f6db2194668191fcbda44661a78cf6d
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2022-07-07 22:18:20 +1200

    Adding configuration to use fzf to move directories

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,4 +10,7 @@ alias ghci "stack ghci"
 # alias for invoice-maker
 alias invoice "~/Projects/Invoice-Maker/venv/bin/python3 ~/Projects/Invoice-Maker/src/main.py"
 
+# alias for fzf directory switching
+alias sd "cd ~ && cd (find * -type d | fzf)"
+
 starship init fish | source
