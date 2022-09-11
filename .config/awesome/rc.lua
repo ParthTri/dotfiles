31e65e1799b3e7e9d459ec8c338c94593d4028ab
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2022-09-11 15:54:10 +1200

    Hyper key in awesome setup

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -62,6 +62,9 @@ editor_cmd = terminal .. " -e " .. editor
 -- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
 
+-- Hyper key
+hyper = "Mod3"
+
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     awful.layout.suit.floating,
