0d6d0047107a2f878e2df9eab171ab05ede3fe7d
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2022-09-12 22:45:45 +1200

    Changing keybinding to new spec

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -37,7 +37,7 @@ keymap('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 keymap('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 keymap('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 keymap('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
-keymap('n', 'gp', '<Cmd>Lspsaga preview_definition<CR>', opts)
+keymap('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 keymap('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
 
 -- Harpoon
