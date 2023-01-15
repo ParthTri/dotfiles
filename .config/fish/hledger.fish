3efa94ce95230f274a7092a5482b5b41bfc2d1b5
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-01-15 21:31:25 +1300

    Adding yearly income statement and changing ledger file

--- .config/fish/hledger.fish
+++ .config/fish/hledger.fish
@@ -1,4 +1,4 @@
-set LEDGER_FILE ~/Work/Accounts/2022.ledger.txt
+set LEDGER_FILE ~/Work/Accounts/Finances.ledger.txt
 
 # Balance Statements
 alias bal "hledger bal -f $LEDGER_FILE"
@@ -6,4 +6,5 @@ alias bal "hledger bal -f $LEDGER_FILE"
 # Income Statements
 alias is "hledger is -p thismonth -f $LEDGER_FILE"
 alias mis "hledger is -M -f $LEDGER_FILE"
+alias yis "hledger is -Y -f $LEDGER_FILE"
 
