b9627ebee9302caff968db8ee41251571585f133
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-01-18 18:01:18 +1300

    Adding function to toggle org heading sizes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -354,16 +354,35 @@
 
 ;; Custom Faces
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.4))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.3))))
- '(org-level-4 ((t (:inherit outline-4 :height 1.2))))
- '(org-level-5 ((t (:inherit outline-5 :height 1.1)))))
+(setq toggle-org-faces-check t)
+
+(defun toggle-org-faces ()
+  "Toggle org level headings whether to be normal font size or in increasing font size."
+  (interactive)
+  (if toggle-org-faces-check
+      (progn
+        (custom-set-faces
+         '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
+         '(org-level-2 ((t (:inherit outline-2 :height 1.4))))
+         '(org-level-3 ((t (:inherit outline-3 :height 1.3))))
+         '(org-level-4 ((t (:inherit outline-4 :height 1.2))))
+         '(org-level-5 ((t (:inherit outline-5 :height 1.1)))))
+        (setq toggle-org-faces-check nil)
+        )
+    (progn
+        (custom-set-faces
+         '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
+         '(org-level-2 ((t (:inherit outline-2 :height 1.2))))
+         '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
+         '(org-level-4 ((t (:inherit outline-4 :height 1.2))))
+         '(org-level-5 ((t (:inherit outline-5 :height 1.2)))))
+        (setq toggle-org-faces-check t)
+        )
+  )
+)
+
+(pt/leader-keys
+  "th" '(toggle-org-faces :which-key "Headings"))
 
 ;; Agenda
 
