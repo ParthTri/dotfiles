f12982331f96fe69500ffbc51c9cff750bab5edb
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-01-29 13:01:24 +1300

    Adding stumpwm configuration

---
+++ .stumpwm.d/init.lisp
@@ -0,0 +1,14 @@
+#-quicklisp
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+
+(in-package :stumpwm)
+(setf *default-package* :stumpwm)
+
+(setf *startup-message* nil)
+
+(load "~/.stumpwm.d/keybindings.lisp")
+(load "~/.stumpwm.d/modeline.lisp")
+
---
+++ .stumpwm.d/keybindings.lisp
@@ -0,0 +1,43 @@
+(in-package :stumpwm)
+
+(which-key-mode)
+
+(stumpwm:set-prefix-key (stumpwm:kbd "s-SPC"))
+
+;; Restart
+(define-key *groups-map* (kbd "r") "restart-soft")
+
+;; Helper function
+(defun tr-define-key (key command)
+  (define-key *top-map* (kbd (concat "s-" key )) command)
+  (define-key *root-map* (kbd key) command))
+
+;; Vim navigation
+(define-key *top-map* (kbd "s-h") "move-focus left")
+(define-key *top-map* (kbd "s-j") "move-focus down")
+(define-key *top-map* (kbd "s-k") "move-focus up")
+(define-key *top-map* (kbd "s-l") "move-focus right")
+
+;; Tab like cycling
+(define-key *top-map* (kbd "s-C-n") "next-in-frame")
+(define-key *top-map* (kbd "s-C-p") "prev-in-frame")
+
+;; Frame cycling
+(define-key *top-map* (kbd "s-C-k") "fnext")
+(define-key *top-map* (kbd "s-C-j") "fprev")
+
+;; Moving Windows
+(define-key *top-map* (kbd "s-H") "move-window left")
+(define-key *top-map* (kbd "s-J") "move-window down")
+(define-key *top-map* (kbd "s-K") "move-window up")
+(define-key *top-map* (kbd "s-L") "move-window right")
+
+;; Splitting Frames
+(tr-define-key "v" "hsplit")
+(tr-define-key "s" "vsplit")
+(tr-define-key "x" "remove-split")
+
+;; Managing splits
+(setf *resize-increment* 25)
+
+(tr-define-key "f" "fullscreen")
