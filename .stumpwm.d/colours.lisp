67725630b1cbadd4bf4310d46c8b6af06e0f5b70
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-02-03 12:53:32 +1300

    Creating colours and theme for stumpwm

---
+++ .stumpwm.d/colours.lisp
@@ -0,0 +1,20 @@
+(in-package :stumpwm)
+
+;; (mapconcat (lambda (colour)
+;;              (format "(defvar pt-%s \"%s\")" (car colour) (cadr colour)))
+;;            colours
+;;            "\n")
+
+(defvar pt-bg "#1d1f21")
+(defvar pt-fg "#c5c8c6")
+(defvar pt-grey "#5a5b5a")
+(defvar pt-red "#cc6666")
+(defvar pt-orange "#de935f")
+(defvar pt-yellow "#f0c674")
+(defvar pt-green "#b5bd68")
+(defvar pt-blue "#81a2be")
+(defvar pt-dark-blue "#41728e")
+(defvar pt-magenta "#c9b4cf")
+(defvar pt-violet "#b294bb")
+(defvar pt-cyan "#8abeb7")
+
---
+++ .stumpwm.d/theme.lisp
@@ -0,0 +1,8 @@
+(in-package :stumpwm)
+
+;; Windows
+(setf *normal-border-width* 1)
+(setf *window-border-style* :tight)
+
+(set-focus-color pt-blue)
+(set-float-focus-color pt-magenta)
