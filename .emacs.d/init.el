4c8cb52129699182b8de1242ef8e6c4e76f6abca
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-02-04 17:51:23 +1300

    Fixing font setting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,7 +14,14 @@
 
 ;; Font
 
-(set-face-attribute 'default nil :font "Jet Brains Mono" :height 90)
+(defun fontify-frame (frame)
+  (set-frame-parameter frame 'font "Jet Brains Mono-9"))
+
+;; Fontify current frame
+(fontify-frame nil)
+
+;; Fontify any future frames
+(push 'fontify-frame after-make-frame-functions)
 
 ;; Line Numbers
 
