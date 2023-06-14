71ec8aea0bf3dd9aa5158d9b584d8ed695c5a695
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-06-14 22:22:02 +1200

    Readding programming support to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -721,6 +721,182 @@
                               (format-time-string "%d%m%Y"))
                       "~/Blog/")))
 
+;; Programming
+
+(define-key prog-mode-map (kbd "C-c e s") #'eglot)
+(define-key prog-mode-map (kbd "C-c e r") #'eglot-reconnect)
+(define-key prog-mode-map (kbd "C-c e a") #'eglot-code-actions)
+(define-key prog-mode-map (kbd "C-c e p") #'flycheck-previous-error)
+(define-key prog-mode-map (kbd "C-c e n") #'flycheck-next-error)
+
+;; Magit
+
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
+  :config
+  (pt/leader-keys
+    "g" '(:ignore g :which-key "git")
+    "gs" '(magit-stage-file :which-key "stage file")
+    "gS" '(magit-stage :which-key "stage all")
+    "gc" '(magit-commit :which-key "commit")
+    "gg" '(magit-status :which-key "status")))
+
+;; Git Gutter
+
+(use-package git-gutter
+  :ensure t
+  :config
+  (global-git-gutter-mode t))
+
+(pt/leader-keys
+  "tg" '(git-gutter-mode :which-key "gutter"))
+
+;; Projectile
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "~/Projects")
+    (setq projectile-project-search-path '("~/Projects")))
+  (setq projectile-switch-project-action 'projectile-dired)
+  (setq projectile-switch-project-action 'neotree-projectile-action)
+
+  ;; (add-to-list 'projectile-globally-ignored-directories "^\\node_modules")
+  )
+
+(pt/leader-keys
+  "p" '(:ignore p :which-key "projects")
+  "pp" '(projectile-switch-project :which-key "switch")
+  "pt" '(projectile-test-project :which-key "test")
+  "pf" '(projectile-find-file :which-key "find")
+  "pr" '(projectile-run-project :whick-key "run")
+  "pc" '(projectile-compile-project :which-key "compile"))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+(use-package persp-mode-projectile-bridge
+  :ensure t
+  :after (persp projectile))
+
+(persp-mode-projectile-bridge-mode)
+
+;; Syntax Checking
+
+(use-package flycheck
+  :ensure t
+  :init
+  (global-flycheck-mode))
+
+;; Python
+
+(use-package elpy
+  :ensure t
+  :defer t
+  :init
+  (advice-add 'python-mode :before 'elpy-enable))
+
+(use-package pyvenv
+  :config
+  (pyvenv-mode 1))
+
+;; Go
+
+(use-package go-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode)))
+
+;; Nim
+
+(use-package nim-mode
+  :ensure t)
+
+;; Web
+
+(use-package web-mode
+  :ensure t)
+
+;; Emmet
+
+(use-package emmet-mode
+  :ensure t
+  :hook ((web-mode . emmet-mode)
+         (js-mode . emmet-mode))
+  :config
+  (setq emmet-move-cursor-between-quotes t))
+
+;; JSX
+
+(use-package rjsx-mode
+  :mode ("\\.js\\'"
+         "\\.jsx\\'")
+  :config
+  (setq js2-mode-show-parse-errors nil
+        js2-mode-show-strict-warnings nil
+        js2-basic-offset 2
+        js-indent-level 2))
+
+;; Typescript
+
+(use-package typescript-mode
+  :ensure t)
+
+;; Svelte
+
+(use-package svelte-mode
+  :ensure t)
+
+;; Terminal
+
+(use-package vterm
+  :ensure t )
+
+(pt/leader-keys
+  "oT" '(vterm :which-key "terminal"))
+
+;; Toggle
+
+(use-package vterm-toggle
+  :ensure t)
+
+(pt/leader-keys
+  "ot" '(vterm-toggle :which-key "terminal"))
+
+;; Comments
+
+(use-package hl-todo
+  :ensure t
+  :hook (prog-mode)
+  :bind (:map hl-todo-mode-map
+              ("C-c t n" . hl-todo-next)
+              ("C-c t p" . hl-todo-previous)
+              ("C-c t l" . hl-todo-occur)))
+
+ (setq hl-todo-keyword-faces
+  '(("TODO"   . "#cc9393")
+    ("FIXME"  . "#cc9393")
+    ("NOTE"   . "#d0bf8f")
+    ("BUG"    . "#8c5353")))
+
+;; Code Folding
+
+(use-package origami
+  :hook (prog-mode))
+
+;; Auto Closing
+
+(add-hook 'prog-mode-hook 'electric-pair-mode)
+
+;; Language Server Protocol
+
+(use-package eglot
+  :ensure t)
+
 ;; CSV
 
 (use-package csv-mode
