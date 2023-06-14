28152a9cdae607912d3703088246a71b38ccd462
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-06-14 22:22:27 +1200

    Adding pnpm to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,7 @@ export PATH="$HOME/.bin/:$PATH"
 export PATH="$HOME/.local/bin/:$PATH"
 export PATH="$HOME/.cargo/bin/:$PATH"
 export PATH="$HOME/.nix-profile/bin:$PATH"
+export PATH="$HOME/.local/share/pnpm:$PATH"
 export NIX_PATH="nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixpkgs:/nix/var/nix/profiles/per-user/root/channels"
 
 source ~/.config/fish/hledger.fish
