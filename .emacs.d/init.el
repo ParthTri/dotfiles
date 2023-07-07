308012352e6f2f3f43ca7fbce1283ca9204d7495
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-07-07 12:07:00 +1200

    Renabling evil mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -226,6 +226,35 @@
   (find-file-other-window "~/Downloads/Cheatsheet-emacs.pdf"))
 (global-set-key (kbd "C-h C-k") 'open-keybind-cheat-sheet)
 
+;; Evil
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+  (define-key evil-normal-state-map (kbd "?") 'replace-regexp)
+
+  ;; exit insert mode by pressing jj quickly
+  (define-key evil-insert-state-map (kbd "C-;") 'evil-normal-state)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
 ;; Persp
 
 (use-package persp-mode
