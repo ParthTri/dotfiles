bc071f9ef4e2659a248ec9823132337d99355611
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-09-07 18:33:55 +1200

    Commenting out quicklsp

--- .stumpwm.d/init.lisp
+++ .stumpwm.d/init.lisp
@@ -1,4 +1,4 @@
-#-quicklisp
+;#-quicklisp
 (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
                                        (user-homedir-pathname))))
   (when (probe-file quicklisp-init)
